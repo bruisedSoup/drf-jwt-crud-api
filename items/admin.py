8b11951bad6262cad64a06d63209e3f088ed90fb
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-04-25 07:35:37 +0800

    Clean up codebase

--- items/admin.py
+++ items/admin.py
@@ -9,19 +9,13 @@ class ItemAdmin(admin.ModelAdmin):
     Visit: http://localhost:8000/admin/items/item/
     """
 
-    # Columns to display in the list view of items
     list_display = ['id', 'name', 'price', 'quantity', 'created_at', 'updated_at']
 
-    # Fields the admin search bar will search in
     search_fields = ['name', 'description']
 
-    # Filter sidebar options (right side of admin list)
     list_filter = ['created_at', 'updated_at']
 
-    # These fields are shown but cannot be edited (auto-managed by Django)
     readonly_fields = ['created_at', 'updated_at']
-
-    # How the form is laid out when adding/editing an item
     fieldsets = (
         ('Basic Information', {
             'fields': ('name', 'description')
@@ -31,6 +25,6 @@ class ItemAdmin(admin.ModelAdmin):
         }),
         ('Timestamps', {
             'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',),  # Collapsed by default
+            'classes': ('collapse',),  
         }),
     )
