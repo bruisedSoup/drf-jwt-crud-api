0aaac2a98ffc754b97fb1a08a778bc7f42b3d34e
bruisedSoup <gonzales.isabella1414@gmail.com>
2026-05-14 13:16:03 +0800

    refactor: integrate Djoser authentication routes

--- items_project/urls.py
+++ items_project/urls.py
@@ -6,6 +6,8 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
